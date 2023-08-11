7e905433c8edb091bfe2f8eaff3fe32f2de192d0
Nitish <neelgiri65@gmail.com>
2023-08-12 03:37:00 +0530

    set3 & 4

---
+++ set3.py
@@ -0,0 +1,61 @@
+# # S1 D3 Assignment - Set 3
+
+# 1. **Tuple Unpacking**: Create a list of tuples, each containing a name and an age. Then, use tuple unpacking to iterate through the list and print each name and age.
+#     - *Input*: [("John", 25), ("Jane", 30)]
+#     - *Output*: "John is 25 years old. Jane is 30 years old."
+
+
+# *******************************************************************************
+
+# 2. **Dictionary Manipulation**: Create a dictionary with keys as names and values as ages. Write functions to add a new name-age pair, update the age of a name, and delete a name from the dictionary.
+#     - *Input*: Add "John": 25, Update "John": 26, Delete "John"
+#     - *Output*: "{}, {'John': 26}, {}"
+
+
+# *******************************************************************************
+
+# 3. **Two Sum Problem**: Given an array of integers and a target integer, find the two integers in the array that sum to the target.
+#     - *Input*: [2, 7, 11, 15], target = 9
+#     - *Output*: "[0, 1]"
+
+
+# *******************************************************************************
+
+# 4. **Palindrome Check**: Write a Python function that checks whether a given word or phrase is a palindrome.
+#     - *Input*: "madam"
+#     - *Output*: "The word madam is a palindrome."
+
+
+# *******************************************************************************
+
+# 5. **Selection Sort**: Implement the selection sort algorithm in Python.
+#     - *Input*: [64, 25, 12, 22, 11]
+#     - *Output*: "[11, 12, 22, 25, 64]"
+
+
+# *******************************************************************************
+
+# 6. **Implement Stack using Queue**: Use Python's queue data structure to implement a stack.
+#     - *Input*: push(1), push(2), pop(), push(3), pop(), pop()
+#     - *Output*: "1, None, 3, None, None"
+
+
+# *******************************************************************************
+
+# 7. **FizzBuzz**: Write a Python program that prints the numbers from 1 to 100, but for multiples of three, print "Fizz" instead of the number, for multiples of five, print "Buzz", and for multiples of both three and five, print "FizzBuzz".
+#     - *Input*: None
+#     - *Output*: "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz, 16,..."
+
+
+# *******************************************************************************
+
+# 8. **File I/O**: Write a Python program that reads a file, counts the number of words, and writes the count to a new file.
+#     - *Input*: A text file named "input.txt" with the content "Hello world"
+#     - *Output*: A text file named "output.txt" with the content "Number of words: 2"
+
+
+# *******************************************************************************
+
+# 9. **Exception Handling**: Write a Python function that takes two numbers as inputs and returns their division, handling any potential exceptions (like division by zero).
+#     - *Input*: 5, 0
+#     - *Output*: "Cannot divide by zero."
---
+++ set4.py
@@ -0,0 +1,72 @@
+# # S1 D4 Assignment - Set 4
+
+# 1. **Anagram Check**: Write a Python function that checks whether two given words are anagrams.
+#     - *Input*: "cinema", "iceman"
+#     - *Output*: "True"
+
+# *******************************************************************************
+
+# 2. **Bubble Sort**: Implement the bubble sort algorithm in Python.
+#     - *Input*: [64, 34, 25, 12, 22, 11, 90]
+#     - *Output*: "[11, 12, 22, 25, 34, 64, 90]"
+
+# *******************************************************************************
+
+# 3. **Longest Common Prefix**: Given a list of strings, find the longest common prefix.
+#     - *Input*: ["flower","flow","flight"]
+#     - *Output*: "fl"
+
+
+# *******************************************************************************
+
+# 4. **String Permutations**: Write a Python function to calculate all permutations of a given string.
+#     - *Input*: "abc"
+#     - *Output*: "['abc', 'acb', 'bac', 'bca', 'cab', 'cba']"
+# 5. **Implement Queue using Stack**: Use Python's stack data structure to implement a queue.
+#     - *Input*: enqueue(1), enqueue(2), dequeue(), enqueue(3), dequeue(), dequeue()
+#     - *Output*: "1, None, 3, None, None"
+# 6. **Missing Number**: Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+#     - *Input*: [3, 0, 1]
+#     - *Output*: "2"
+# 7. **Climbing Stairs**: You are climbing a staircase. It takes n steps to reach the top. Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+#     - *Input*: 3
+#     - *Output*: "3"
+# 8. **Invert Binary Tree**: Invert a binary tree (mirroring it).
+#     - *Input*: A binary tree
+#     - *Output*: Inverted binary tree
+# 9. **Power of Two**: Given an integer, write a function to determine if it is a power of two.
+#     - *Input*: 16
+#     - *Output*: "True"
+# 10. **Contains Duplicate**: Given an array of integers, find if the array contains any duplicates.
+#     - *Input*: [1, 2, 3, 1]
+#     - *Output*: "True"
+# 11. **Binary Search**: Write a function that implements binary search on a sorted array.
+#     - *Input*: [1, 2, 3, 4, 5, 6], target = 4
+#     - *Output*: "3"
+# 12. **Depth First Search (DFS)**: Implement DFS for a graph in Python.
+#     - *Input*: A graph
+#     - *Output*: Vertices visited in DFS order
+# 13. **Breadth First Search (BFS)**: Implement BFS for a graph in Python.
+#     - *Input*: A graph
+#     - *Output*: Vertices visited in BFS order
+# 14. **Quick Sort**: Implement quick sort in Python.
+#     - *Input*: [10, 7, 8, 9, 1, 5]
+#     - *Output*: "[1, 5, 7, 8, 9, 10]"
+# 15. **Single Number**: Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+#     - *Input*: [4,1,2,1,2]
+#     - *Output*: "4"
+# 16. **Palindrome Linked List**: Given a singly linked list, determine if it is a palindrome.
+#     - *Input*: [1,2,2,1]
+#     - *Output*: "True"
+# 17. **Implement Trie (Prefix Tree)**: Implement a trie with insert, search, and startsWith methods.
+#     - *Input*: insert("apple"), search("apple"), startsWith("app")
+#     - *Output*: "True, True"
+# 18. **Maximum Subarray**: Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+#     - *Input*: [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+#     - *Output*: "6"
+# 19. **Implement Stack using Linked List**: Use Python's linked list data structure to implement a stack.
+#     - *Input*: push(1), push(2), pop(), push(3), pop(), pop()
+#     - *Output*: "1, None, 3, None, None"
+# 20. **Basic Django Web App**: Create a basic web application using Django that has two routes, one that displays "Hello, World!" and one that displays "Goodbye, World!".
+#     - *Input*: Visit "/", Visit "/goodbye"
+#     - *Output*: "Hello, World!", "Goodbye, World!"
